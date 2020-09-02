77366eb297c5e0a7ae8ce6454cd03810efb511e4
Witchzell <pw.chiarioni@gmail.com>
2020-09-02 16:37:47 +1200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Find out nutrisional value of Mcdonals food">
+    <meta name="author" content="177013 Squad">
+    <title>McDiabetus</title>
+    <!--linking to stylesheet-->
+    <link rel="stylesheet" href="CSS/Page_Style.css">
+</head>
+<body>
+    <header></header>
+</body>
+</html>
