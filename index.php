ca3c3a2ff5be4724eefaa9637e4aa2a312e0b2e4
Witchzell <pw.chiarioni@gmail.com>
2020-09-02 18:14:31 +1200

    Improvements
    still need refining

--- index.php
+++ index.php
@@ -14,9 +14,37 @@
 </head>
 
 <body>
+    <!--header start-->
     <header>
         <img src="IMG/McArch.png" id="logo">
         <h2>I'm loving it</h2>
     </header>
+    <!--header finihed-->
+    
+    <!--body start-->
+    <h1>About</h1>
+    <div class="gap">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus consequat odio 
+            a pellentesque. Nam dapibus vitae arcu at interdum. Fusce tempus nisi urna, non blandit 
+            libero fermentum nec. Cras sit amet velit ac ante pellentesque ornare eget vel nulla. 
+            Phasellus mollis, ipsum vitae faucibus ultricies, eros sapien gravida neque.
+        </p>
+    </div>
+    <h1>Database</h1>
+
+    <!--database box-->
+    <div id="box">
+        <p>sample text</p>
+    </div>
+    <!--body end-->
+
+    <br/><br/>
+
+    <footer>
+        <p>
+            &#169; 2020 177013 Squad
+        </p>
+    </footer>
 </body>
 </html>
